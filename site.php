c7436089bd9e0a630a69fda3bc5282030269842c
hanniton <hanniton123@gmail.com>
2025-04-13 07:59:48 +0200

    Initial commit

---
+++ site.php
@@ -0,0 +1,13 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> First PHP</title>
+</head>
+        <body>
+
+            <?php
+echo "<h1>welcome to my first php site</h1>";
+echo "<hr>";
+echo"<p> dfhjgkgjsgfkjsbdfg.</p>";
+?>
